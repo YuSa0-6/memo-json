d212fc86d255997404ad2964a349bdb87eb0feb4
YuSa0-6 <105704944+YuSa0-6@users.noreply.github.com>
2024-02-08 17:05:10 +0900

    Revert "Develop"

--- models/memo.rb
+++ models/memo.rb
@@ -6,7 +6,4 @@ class Memo
   def all
     File.open(FILE_PATH) { |file| JSON.parse(file.read) }
   end
-  def create(memos)
-    File.open(FILE_PATH, 'w') { |file| JSON.dump(memos, file) }
-  end
 end
